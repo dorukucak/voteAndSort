ff282a19d49e72f096d29d0ed048c9528f952b8b
dorukucak <84201421+dorukucak@users.noreply.github.com>
2021-06-17 15:06:03 +0300

    listing OK

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import './App.css';
 import {BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom';
+import logo from './hepsiburada-logo.png';
 // import Modal from 'react-modal';
 // Modal.setAppElement('#app');
 
@@ -35,7 +37,7 @@ class App extends React.Component {
       return 'This option already exists';
     }
     this.setState((prevState) => ({
-      options: prevState.options.concat(option)
+      options: prevState.options.concat([option])
     }));
   };
 
@@ -63,7 +65,7 @@ class App extends React.Component {
 
  render() { return (
       <BrowserRouter>
-      <div>
+      <div className='outer'>
           <Header />        
           <Switch>
               <Route path="/" render={() => 
@@ -111,13 +113,15 @@ class AddOption extends React.Component {
     };
     handleAddOption = (e) => {
       e.preventDefault();
-      const option = e.target.elements.option.value.trim();
+      const title = e.target.elements.title.value.trim();
+      const link =  e.target.elements.link.value.trim();
+      const option = [title, link]
       const error = this.props.handleAddOption(option);
   
       this.setState(() => ({ error }));
   
       if (!error) {
-        e.target.elements.option.value = '';
+        e.target.elements.title.value = '';
       }
     };
     render() {
@@ -125,7 +129,8 @@ class AddOption extends React.Component {
         <div>
           {this.state.error && <p className='add-option-error'>{this.state.error}</p>}
           <form className='add-option' onSubmit={this.handleAddOption}>
-            <input className='add-option__input' type="text" name="option" />
+            <input className='add-option__input' type="text" name="title" />
+            <input className='add-option__link' type="text" name="link" />
             <button className='button'>Add Option</button>
           </form>
         </div>
@@ -137,21 +142,16 @@ class AddOption extends React.Component {
   
     <div className='header'>
       <div className='container'>
-      <h1 className='header__title'>{props.title}</h1>
-      {props.subtitle && <h2 className='header__subtitle'>{props.subtitle}</h2>}
-      </div>
-      
+      <img src={logo} alt="hepsiburada-logo" width="200" height="125"/>
+      </div>      
     </div>
   
-);
+  );
 
-Header.defaultProps = {
-  title: 'Hepsiburada'
-};
 
 const Option = (props) => (
   <div className='option'>
-    <p className='option__text'>{props.count}.{props.optionText}</p>
+    <p className='option__text'>{props.count}.{props.optionText} {props.link}</p>
     <button className='button button--link'
       onClick={(e) => {
         props.handleDeleteOption(props.optionText);
@@ -182,19 +182,14 @@ const Option = (props) => (
 
 
 const Options = (props) =>  (
-  <div>
-    <div className='widget-header'>
-      <h3 className='widget-header__title'>Your Options</h3>
-      <button className='button button--link'
-      onClick={props.handleDeleteOptions}>Remove All</button>
-    </div>
-  
-  {props.options.length === 0 && <p className='widget__message'>Please add an option to get started!</p>}
+  <div>  
+  {props.options.length === 0 && <p className='widget__message'>Please submit a link to get started!</p>}
   {
     props.options.map((option, index) => (
       <Option 
-        key={option}
-        optionText={option}
+        key={option[0]}
+        optionText={option[0]}
+        link={option[1]}
         count={index + 1}
         handleDeleteOption={props.handleDeleteOption}
       />
@@ -206,13 +201,13 @@ const Options = (props) =>  (
 class Main extends React.Component {
   
   render() {
-    const subtitle = 'Put your life in the hands of a computer';
-
     return (
-      <div>      
+      <div className='main'>      
        
         <div className='container'>
-        <NavLink to="/create" activeClassName="is-active">SUBMIT A LINK</NavLink>
+        <Link className='big-button' to="/create" activeClassName="is-active">
+        <button className='big-button'>SUBMIT A LINK</button>
+        </Link>
         <div className='widget'>
       <Options
       options={this.props.options}
